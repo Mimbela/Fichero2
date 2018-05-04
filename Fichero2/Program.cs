1c0ca5bab1ed07e203ac5ff246cdf45c96aa6f81
senin8 <malizziaaries@gmail.com>
2018-05-04 12:40:27 +0200

    Add project files.

---
+++ Fichero2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fichero2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter fichero;
+            fichero = File.CreateText("RegistrodeUsuario.txt");//creamos el fichero
+
+            string frase = " "; //inicializamos la frase
+
+            do
+            {
+                Console.WriteLine("Introduce una frase: ");
+                frase = Console.ReadLine();
+
+                if (frase.ToUpper() != "FIN")
+                    fichero.WriteLine(frase);
+                 
+            } while (frase.ToUpper() != "FIN");
+
+        }
+    }
+}
+
+
+//hacer que el usuario ponga varias veces una frase hasta que escriba la palabra fin
