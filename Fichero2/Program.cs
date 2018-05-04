7e62a270de74935e73f700cda676b0ed033ace6e
senin8 <malizziaaries@gmail.com>
2018-05-04 13:41:43 +0200

    actualizacion

--- Fichero2/Program.cs
+++ Fichero2/Program.cs
@@ -11,24 +11,98 @@ namespace Fichero2
     {
         static void Main(string[] args)
         {
-            StreamWriter fichero;
-            fichero = File.CreateText("RegistrodeUsuario.txt");//creamos el fichero
+            //************hacer que el usuario ponga varias veces una frase hasta que escriba la palabra fin
+            //*************creamos el fichero STREAMWRITER
+            //StreamWriter fichero;
+            //fichero = File.CreateText("RegistrodeUsuario.txt");
 
-            string frase = " "; //inicializamos la frase
+            //string frase = " "; //*****************inicializamos la frase
 
-            do
-            {
-                Console.WriteLine("Introduce una frase: ");
-                frase = Console.ReadLine();
+            //do
+            //{
+            //    Console.WriteLine("Introduce una frase: ");
+            //    frase = Console.ReadLine();
 
-                if (frase.ToUpper() != "FIN")
-                    fichero.WriteLine(frase);
-                 
-            } while (frase.ToUpper() != "FIN");
+            //    if (frase.ToUpper() != "FIN")
+            //        fichero.WriteLine(frase);
+
+            //} while (frase.ToUpper() != "FIN");
+            //fichero.Close();
+            //*******************************************************************************************************************************************
+
+            //*****LEEMOS EL FICHERO STREAMREADER POR LÍNEAS
+
+            //StreamReader fichero;
+            //string linea;
+
+            //fichero = File.OpenText("RegistrodeUsuario.txt");
+            //linea = fichero.ReadLine();
+            //Console.WriteLine(linea);
+            //Console.WriteLine(fichero.ReadLine());
+            //Console.WriteLine(fichero.ReadLine());
+
+            //fichero.Close();
+
+            //Console.ReadKey();
+
+            //************LEEMOS TODO EL FICHERO ENTERO Y NO POR LÍNEAS
+
+            //StreamReader fichero;
+            //string linea;
+
+            //fichero = File.OpenText("RegistrodeUsuario.txt");
+            ////si está en otra ruta ponemos @"c:\ejemplos\fichero1.txt ó @"..\ejemplos\fichero1.txt"
+
+            //do
+            //{
+            //    linea = fichero.ReadLine();
+            //    if (linea != null)
+            //        Console.WriteLine(linea);
+
+
+            //} while (linea != null);
+            //fichero.Close();
+            //Console.ReadKey();
+
+            //****************AÑADIMOS MAS LÍNEAS AL REGISTRO DE USUARIO SOBRE UN FICHERO QUE YA EXISTE
+
+            //StreamWriter fichero;
+            //fichero = File.AppendText("RegistrodeUsuario.txt");
+            //fichero.WriteLine("otra linea mas");
+
+            //fichero.Close();
+
+            //**************************SI ABRO UN FICHERO Y PUEDA QUE NO EXISTA
+            //****************************SIN TERMINAR
+
+            //try
+            //{
+
+            //StreamReader fichero;
+            //string linea;
+
+            //fichero = File.OpenText("RegistrodeUsuario.txt");
+            
+
+            //do
+            //{
+            //    linea = fichero.ReadLine();
+            //    if (linea != null)
+            //        Console.WriteLine(linea);
+
+
+            //} while (linea != null);
+            //fichero.Close();
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+            //Console.ReadKey();
 
         }
     }
 }
 
 
-//hacer que el usuario ponga varias veces una frase hasta que escriba la palabra fin
